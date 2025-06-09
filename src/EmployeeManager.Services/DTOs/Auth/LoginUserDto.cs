1ff31f04fde6c93971eb33193df3d63965980744
s30422 <s30422@pjwstk.edu.pl>
2025-06-09 20:16:39 +0200

    Don't validate login and passwod during authentication

--- src/EmployeeManager.Services/DTOs/Auth/LoginUserDto.cs
+++ src/EmployeeManager.Services/DTOs/Auth/LoginUserDto.cs
@@ -5,10 +5,8 @@ namespace EmployeeManager.Services.dtos.auth;
 public class LoginUserDto
 {
     [Required]
-    [RegularExpression(@"^[^\d][\w\d_]{2,}$")]
     public string Login { get; set; }
     
     [Required]
-    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$")]
     public string Password { get; set; }
 }
